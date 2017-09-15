c2ec8711d77b6982854f123bbe841b0377607542
merryymustard <merryymustard@gmail.com>
2017-09-15 13:34:21 +0200

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,37 @@
+
+var my_mail= "v@g.com";
+var myPassword= "123456"
+
+
+var aButtonVar= document.getElementById("enter_button");
+
+aButtonVar.onclick = function (){
+	//alert(document.getElementById ("fld_email").value);
+	//document.getElementById("fld_email").value = "mymail@mail.com";
+	//return false;
+
+
+
+	var fldMail = document.getElementById("fld_email");
+	var email = fldMail.value;
+
+	var password = document.getElementById("fld_password").value;
+
+
+	//alert("the email: " + email + " the pass: " + password);
+
+	if(email != my_mail){
+		console.log("mail incorrect");
+		alert("mail incorrect");
+		return false;
+
+	} else if (password ! == myPassword){
+
+
+
+		console.log("password incorrect");
+		alert("pass incorrect");
+		return false;
+	}
+	else
+	
